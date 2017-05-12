628fe5c645615bc1a79c519f3ec14f34b87d848f
Nick Shayan <N.Shayan@me.com>
2017-05-12 14:04:33 +0200

    Update with working build and all necessary deps, except NDK/SDK. Use Genymotion for VM or download SDK.

--- TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
@@ -13,10 +13,10 @@ public class BuildVars {
     public static boolean DEBUG_PRIVATE_VERSION = false;
     public static int BUILD_VERSION = 957;
     public static String BUILD_VERSION_STRING = "3.18";
-    public static int APP_ID = 0; //obtain your own APP_ID at https://core.telegram.org/api/obtaining_api_id
-    public static String APP_HASH = ""; //obtain your own APP_HASH at https://core.telegram.org/api/obtaining_api_id
-    public static String HOCKEY_APP_HASH = "your-hockeyapp-api-key-here";
-    public static String HOCKEY_APP_HASH_DEBUG = "your-hockeyapp-api-key-here";
+    public static int APP_ID = 152333; //obtain your own APP_ID at https://core.telegram.org/api/obtaining_api_id
+    public static String APP_HASH = "0ef9cce77c815023801fc427db847f20"; //obtain your own APP_HASH at https://core.telegram.org/api/obtaining_api_id
+    public static String HOCKEY_APP_HASH = "0b75ccdda26a4d9291127d323953c100";
+    public static String HOCKEY_APP_HASH_DEBUG = "0b75ccdda26a4d9291127d323953c100";
     public static String BING_SEARCH_KEY = ""; //obtain your own KEY at https://www.bing.com/dev/en-us/dev-center
     public static String FOURSQUARE_API_KEY = ""; //obtain your own KEY at https://developer.foursquare.com/
     public static String FOURSQUARE_API_ID = ""; //obtain your own API_ID at https://developer.foursquare.com/
