766defeeac5891cb9f8c3952e85acd6441694ae0
Nick Shayan <N.Shayan@me.com>
2017-08-20 19:48:05 +0200

    Added unit tests, mockito tests.

--- TMessagesProj/src/main/java/org/telegram/Test/CallingCodeInfoTest.java
+++ TMessagesProj/src/main/java/org/telegram/Test/CallingCodeInfoTest.java
@@ -1,8 +1,59 @@
+package org.telegram.Test;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.telegram.PhoneFormat.CallingCodeInfo;
+
 import static org.junit.Assert.*;
 
 /**
  * Created by ns on 20/08/2017.
  */
 public class CallingCodeInfoTest {
+
+    CallingCodeInfo cci = new CallingCodeInfo();
+    String code1;
+    String code2;
+    String code3;
+
+    @Before
+    public void setUp() throws Exception {
+//        code1 = "+12025550121";
+//        code2 = "12025550121";
+//        code3 = "120 255 50 121";
+        code1 = "0612345678";
+//        code2 = "+31612345678";
+//        code3 = "003112345678";
+        code2 = "+31 12345678";
+        code3 = "yyyyy";
+    }
+
+    /**
+     *
+     * @throws Exception
+     */
+    @Test
+    public void format() throws Exception {
+        String result1 = cci.format(code1);
+        String result2 = cci.format(code2);
+        String result3 = cci.format(code3);
+        System.out.println(result1 + "\n" + result2 + "\n" + result3);
+    }
+
+    /**
+     * Status: OK. Does not validate anything for some reason.
+     * @throws Exception
+     */
+    @Test
+    public void isValidPhoneNumber() throws Exception {
+        Boolean result1 = cci.isValidPhoneNumber(code1);
+        Boolean result2 = cci.isValidPhoneNumber(code2);
+        Boolean result3 = cci.isValidPhoneNumber(code3);
+        System.out.println(result1 + " " + result2 + " " + result3);
+        Assert.assertTrue(result1);
+        Assert.assertTrue(result2);
+        Assert.assertTrue(result3);
+    }
 
 }
--- TMessagesProj/src/main/java/org/telegram/Test/ContactsControllerTest.java
+++ TMessagesProj/src/main/java/org/telegram/Test/ContactsControllerTest.java
@@ -1,18 +1,88 @@
 package org.telegram.Test;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+import org.telegram.messenger.ContactsController;
+import org.telegram.tgnet.TLRPC;
+
+
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 /**
  * Created by ns on 20/08/2017.
  */
 
-public class UtilitiesTest {
-    public UtilitiesTest() {
+public class ContactsControllerTest {
+
+//    @Mock
+    ContactsController cc = mock(ContactsController.class);
+
+//    @Rule public MockitoRule mockitoRule = MockitoJUnit.rule();
+
+    TLRPC.User user;
+
+    @Before
+    public void setup() {
+        user = new TLRPC.User();
+        user.id = 33973748;
+        user.first_name = "First";
+        user.last_name = "Last";
+        user.username = "testson12";
+        user.phone = "+12025550121";
+    }
+
+//    TLRPC.TL_contact contact = new
+
+    /**
+     * Status: OK.
+     * @throws Exception
+     */
+    @Test
+    public void testAddContactBehavior() throws Exception {
+        // Execute behavior
+        cc.addContact(user);
+        // Verify the behavior
+        verify(cc).addContact(user);
+
+        cc.readContacts();
+//        cc.contacts.size();
+//        Assert.assertEquals(runCc.contacts.get(0).user_id, user.id);
+        testAddContact();
+    }
+
+    /**
+     * Status: Does not work for some reason. Telegram's too spaghetti.
+     * @throws Exception
+     */
+    @Test
+    public void testAddContact() throws Exception {
+        ContactsController run = new ContactsController();
+        run.addContact(user);
+        run.readContacts();
+        run.contacts.size();
+//        Assert.assertEquals(runCc.contacts.get(0).user_id, user.id);
+    }
+
+    /**
+     * Status: OK.
+     * @throws Exception
+     */
+    @Test
+    public void testFormatName() throws Exception {
+        String result = ContactsController.formatName(user.first_name, user.last_name);
+        String name = "First Last";
+        System.out.println(result);
+        Assert.assertEquals(name, result);
     }
 
     @Test
-    public void testSomething() {
-        
+    public void testMath() {
+        assertEquals(1, 1);
     }
 }
